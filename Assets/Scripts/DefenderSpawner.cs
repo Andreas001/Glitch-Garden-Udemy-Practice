6b7d279e8cd9b8488a5c3e0145b09b4ab81dfa8a
Andreas Geraldo <andreasgeraldo0@gmail.com>
2021-01-11 18:06:18 +0700

    Added spawning defenders
    Added spawning defenders and being able to choose from which defenders to spawn. Though I am discontinuing this practice project and instead decided to move on to tilevania.

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 
 public class DefenderSpawner : MonoBehaviour
 {
-    [SerializeField] GameObject defender;
+    Defender defender;
 
     private void OnMouseDown() {
         SpawnDefender(GetSquareClicked());
     }
 
+    public void SetSelectedDefender(Defender defender) {
+        this.defender = defender;
+    }
+
     private Vector2 GetSquareClicked() {
         Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
-        return worldPos;
+        Vector2 gridPos = SnapToGrid(worldPos);
+        return gridPos;
+    }
+
+    private Vector2 SnapToGrid(Vector2 rawWorldPos) {
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+
+        return new Vector2(newX, newY);
     }
 
     private void SpawnDefender(Vector2 position) {
-        GameObject newDefender = Instantiate(defender, position, Quaternion.identity) as GameObject;
+        Defender newDefender = Instantiate(defender, position, Quaternion.identity) as Defender;
     }
 }
